efb8bac14a3bfcb0359bc6df8b09ef6cb22df2e8
guilherme amorim <cezarguilherme03@gmail.com>
2024-05-18 15:28:24 -0300

    finalizado

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,7 @@
 // import Container from './components/Container/Container';
 import Container from 'components/Container/Container';
 import React from 'react';
-import {light} from '../../assets/Icons/Combined Shape.svg'
-import {dark} from '../../assets/Icons/Combined Shape (1).svg'
+
 
 const Header = ({
 titleHeader
@@ -15,10 +14,7 @@ titleHeader
                     {titleHeader}
                 </h1>
 
-                <div>
-                    <img src={light}/>
-                    <img src={dark}/>
-                </div>
+
             </section>
         </Container>
         </>
